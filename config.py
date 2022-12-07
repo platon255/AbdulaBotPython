f515ed582763afbf425f308808df4ad21973a25b
baton4ek <96630634+platon255@users.noreply.github.com>
2022-12-07 20:50:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "b4659059ca82a1f4b1fe27a3cae142d2"
-tg_bot_token = "5767918329:AAEGzVtldbLIkMCZYO3dI20iQ474S38rCNA"
+open_weather_token = "your open weather token"
+tg_bot_token = "yout tg bot token"
